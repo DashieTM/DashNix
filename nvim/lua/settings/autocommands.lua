7fb4f9ee73241ffb2dab50d649fa9725641d499d
Fabio Lenherr <fabio.lenherr@gmail.com>
2022-08-31 00:27:23 +0200

    feat: eww hypr alacritty and more

--- nvim/lua/settings/autocommands.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
-})
--- nvim/lua/settings/vimtex.lua
+++
@@ -1,8 +0,0 @@
---vim.cmd('let g:vimtex_view_method = "zathura"')
-
-vim.cmd('let g:vimtex_view_general_viewer = "okular"')
---let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
-
-vim.cmd('let g:vimtex_compiler_method = "latexmk"')
-
---let maplocalleader = ","
