a54e94bbb0d57491f0ab6cce0b5b32459a63e43c
Fabio Lenherr <fabio.lenherr@gmail.com>
2022-09-19 15:06:41 +0200

    refresh dotfiles

---
+++ eww/scripts/ncspot_control.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+NUM=$(pactl list clients short | grep "ncspot" | awk -F 'PipeWire' ' { print $1 } ' | tr -d ' \t\n')
+CLIENT=$(pactl list sink-inputs short | grep "$NUM" | awk -F ' ' ' { print $1 }' | tr -d ' \t\n')
+pactl set-sink-input-volume "$CLIENT" "$1"
