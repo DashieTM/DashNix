5bb44594bbf767c65fa3ecac767c282f11764844
Fabio Lenherr <fabio.lenherr@gmail.com>
2023-01-03 09:20:42 +0100

    feat: differentiate between laptop and desktop

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
-find $PWD -maxdepth 10 -mindepth 1 -type d -exec ln -s '{}' $HOME/.config/ \;
+find $PWD -maxdepth 1 -mindepth 0 -exec ln -s '{}' $HOME/.config/ \;
 mv $HOME/.config/.zshrc ../.zshrc
 unlink $HOME/.config/.git 
 unlink $HOME/.config/.gitignore
 unlink $HOME/.config/README.md
+unlink $HOME/.config/eww_desktop
+unlink $HOME/.config/hypr_desktop
+unlink $HOME/.config/eww_laptop
+unlink $HOME/.config/hypr_laptop
+unlink $HOME/.config/setup.sh
+unlink $HOME/.config/dotFiles
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+if [ "$1" = "laptop" ]; then
+  ln -s $PWD/eww_laptop $HOME/.config/eww
+  ln -s $PWD/hypr_laptop $HOME/.config/hypr
+elif [ "$1" = "desktop" ]; then 
+  ln -s $PWD/eww_desktop $HOME/.config/eww
+  ln -s $PWD/hypr_desktop $HOME/.config/hypr
+fi
