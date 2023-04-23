090314c4e58b0ade2848d868b7346e71e6667e2f
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2023-04-23 10:56:45 +0200

    fix: Terminal popup

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -158,4 +158,18 @@ return {
       require("neoscroll.config").set_mappings(t)
     end,
   },
+  {
+    "simrat39/rust-tools.nvim",
+    lazy = true,
+    event = "FileType rust",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
+    opts = function()
+      local extension_path = vim.env.HOME .. "/.local/share/nvim/mason/packages/codelldb/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+      return {
+        dap = { adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path) },
+      }
+    end,
+  },
 }
