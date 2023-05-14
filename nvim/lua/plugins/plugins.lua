3f59accc899f3c6fcf532b326eb5542f61eab476
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2023-05-15 01:03:59 +0200

    chore: Remove unused plugin

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -169,20 +169,6 @@ return {
     end,
   },
   {
-    "simrat39/rust-tools.nvim",
-    lazy = true,
-    event = "FileType rust",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
-    opts = function()
-      local extension_path = vim.env.HOME .. "/.local/share/nvim/mason/packages/codelldb/extension/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-      return {
-        dap = { adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path) },
-      }
-    end,
-  },
-  {
     "kaarmu/typst.vim",
     lazy = true,
     event = "FileType typst",
