e03b1b9997755e1d0bdc131be763b2bb9489d119
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2023-10-04 20:08:07 +0200

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,19 @@
 find $PWD -maxdepth 1 -mindepth 0 -exec ln -s '{}' $HOME/.config/ \;
 mv $HOME/.config/.zshrc ../.zshrc
-unlink $HOME/.config/.git 
+unlink $HOME/.config/.git
 unlink $HOME/.config/.gitignore
 unlink $HOME/.config/README.md
-unlink $HOME/.config/eww_desktop
 unlink $HOME/.config/hypr_desktop
-unlink $HOME/.config/eww_laptop
 unlink $HOME/.config/hypr_laptop
 unlink $HOME/.config/setup.sh
 unlink $HOME/.config/dotFiles
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3' && gsettings set org.gnome.desktop.interface color-scheme 'default'
+sh ./pac load
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 if [ "$1" = "laptop" ]; then
-  ln -s $PWD/eww_laptop $HOME/.config/eww
-  ln -s $PWD/hypr_laptop $HOME/.config/hypr
-elif [ "$1" = "desktop" ]; then 
-  ln -s $PWD/eww_desktop $HOME/.config/eww
-  ln -s $PWD/hypr_desktop $HOME/.config/hypr
+	ln -s $PWD/hypr_laptop $HOME/.config/hypr
+elif [ "$1" = "desktop" ]; then
+	ln -s $PWD/hypr_desktop $HOME/.config/hypr
 fi
