293c4965df99ead3c855a425cf56cf40e3eac8f6
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2023-11-18 00:55:43 +0100

    chore: Remove neovim clutter

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,6 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
-      -- options for vim.diagnostic.config()
       diagnostics = {
         underline = true,
         update_in_insert = false,
@@ -22,101 +21,17 @@ return {
       -- LSP Server Settings
       ---@type lspconfig.options
       servers = {
-        jsonls = {},
-        tailwindcss = {},
         cssls = {},
         html = {},
-        clangd = {
-          root_pattern = {
-            ".clangd",
-            ".clang-tidy",
-            ".clang-format",
-            "compile_commands.json",
-            "compile_flags.txt",
-            "configure.ac",
-            ".git",
-            "build/compile_commands.json",
-          },
-          filetypes = {
-            "c",
-            "cpp",
-            "objc",
-            "objcpp",
-            "cuda",
-            "proto",
-            "cl",
-          },
-        },
-        pyright = {},
-        cmake = {},
         bashls = {},
         ansiblels = {},
-        marksman = {},
         asm_lsp = {},
-        omnisharp = {
-          handlers = {
-            ["textDocument/definition"] = function(...)
-              return require("omnisharp_extended").handler(...)
-            end,
-          },
-          inlayHintsOptions = {
-            enableForParameters = true,
-            forLiteralParameters = true,
-            forIndexerParameters = true,
-            forObjectCreationParameters = true,
-            forOtherParameters = true,
-            suppressForParametersThatDifferOnlyBySuffix = false,
-            suppressForParametersThatMatchMethodIntent = false,
-            suppressForParametersThatMatchArgumentName = false,
-            enableForTypes = true,
-            forImplicitVariableTypes = true,
-            forLambdaParameterTypes = true,
-            forImplicitObjectCreation = true,
-          },
-          enable_roslyn_analyzers = true,
-          organize_imports_on_format = true,
-          enable_import_completion = true,
-        },
-        rnix = {},
-        rust_analyzer = {
-          diagnostics = {
-            enable = true,
-            experimental = true,
-          },
-        },
-        tsserver = {
-          settings = {
-            typescript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-          },
-        },
         typst_lsp = {
           settings = {
             experimentalFormatterMode = "on",
             exportPdf = "onSave",
           },
         },
-        texlab = {},
         ltex = {
           settings = {
             ltex = {
@@ -157,11 +72,6 @@ return {
             },
           },
         },
-      },
-      setup = {
-        jdtls = function()
-          return true
-        end,
       },
     },
     init = function()
