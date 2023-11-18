4d0c5f7db780deb7230f20fba8339180bac2375c
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2023-11-18 01:03:39 +0100

    feat: Use folke edgy, that dude is insane

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -217,4 +217,49 @@ return {
       { "<A-F>", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
     },
   },
+  {
+    "folke/edgy.nvim",
+    opts = {
+      animate = {
+        enabled = false,
+      },
+      left = {},
+      right = {
+        -- Neo-tree filesystem always takes half the screen height
+        {
+          title = "Neo-Tree",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          size = { height = 0.5 },
+        },
+        {
+          title = "Neo-Tree Git",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "git_status"
+          end,
+          pinned = true,
+          open = "Neotree position=right git_status",
+        },
+        {
+          title = "Neo-Tree Buffers",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "buffers"
+          end,
+          pinned = true,
+          open = "Neotree position=top buffers",
+        },
+        {
+          ft = "Outline",
+          pinned = true,
+          open = "SymbolsOutlineOpen",
+        },
+        -- any other neo-tree windows
+        "neo-tree",
+      },
+    },
+  },
 }
