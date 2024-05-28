5ed1986caa04440548b9d7e149f55ceed344c858
DashieTM <fabio.lenherr@gmail.com>
2024-05-28 13:14:48 +0200

    hyprlock: fix config

--- nix/programs/hyprland/hyprlock.nix
+++ nix/programs/hyprland/hyprlock.nix
@@ -1,35 +1,36 @@
-{ pkgs
-, inputs
-, config
+{ config
 , ...
 }:
 {
-  programs.hyprlock.enable = true;
-  programs.hyprlock.settings = {
-    backgrounds = [
-      {
-        monitor = "";
-        path = "";
-        color = "rgba(26, 27, 38, 1.0)";
-      }
-    ];
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      background = [
+        {
+          monitor = "";
+          path = "";
+          color = "rgba(26, 27, 38, 1.0)";
+        }
+      ];
 
-    input-fields = [
-      {
-        monitor = "${config.programs.ironbar.monitor}";
+      input-field = [
+        {
+          monitor = "${config.programs.ironbar.monitor}";
 
-        placeholder_text = "password or something";
-      }
-    ];
+          placeholder_text = "password or something";
+        }
+      ];
 
-    labels = [
-      {
-        monitor = "";
-        text = "$TIME";
-        font_size = 50;
-        valign = "center";
-        halign = "center";
-      }
-    ];
+      label = [
+        {
+          monitor = "${config.programs.ironbar.monitor}";
+          text = "$TIME";
+          font_size = 50;
+          position = "0, 200";
+          valign = "center";
+          halign = "center";
+        }
+      ];
+    };
   };
 }
