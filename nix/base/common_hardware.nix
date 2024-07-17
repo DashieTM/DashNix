dba12b15b8791d2311a3f6e46938cd0891d68716
DashieTM <fabio.lenherr@gmail.com>
2024-07-17 23:32:11 +0200

    Add readme

--- nix/base/common_hardware.nix
+++ nix/base/common_hardware.nix
@@ -41,7 +41,6 @@ in
   };
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
--- scripts/README.md
+++
@@ -1,7 +0,0 @@
-# Packages 
-
-All packages used are within the respective text files:
-
-- pacman -> pacmanpkg.txt
-- crates.io -> carpkg.txt
-- flatpak -> flatpkg.txt
