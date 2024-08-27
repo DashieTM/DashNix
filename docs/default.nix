cc67ca420cd978160dca005d765dd33dd4878d3e
DashieTM <fabio.lenherr@gmail.com>
2024-08-28 00:07:27 +0200

    Add docs

--- docs/default.nix
+++ docs/default.nix
@@ -1,7 +1,22 @@
+# with friendly help by stylix: https://github.com/danth/stylix/blob/master/docs/default.nix
 { pkgs, build_systems, ... }:
 
 let
-  makeOptionsDoc = configuration: pkgs.nixosOptionsDoc { inherit (configuration) options lib; };
+  makeOptionsDoc =
+    configuration:
+    pkgs.nixosOptionsDoc {
+      inherit (configuration) options;
+
+      # Filter out any options not beginning with `stylix`
+      transformOptions =
+        option:
+        option
+        // {
+          visible =
+            option.visible
+            && (builtins.elemAt option.loc 0 == "conf" || builtins.elemAt option.loc 0 == "mods");
+        };
+    };
   example = makeOptionsDoc (build_systems [ "example" ] ../example/.)."example";
 in
 pkgs.stdenvNoCC.mkDerivation {
---
+++ docs/src/SUMMARY.md
@@ -0,0 +1,2 @@
+[Options](dashNix.md)
+
