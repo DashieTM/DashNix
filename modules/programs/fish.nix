3a2266d44ff3c39b111f13a5d24c386845b3d9ec
DashieTM <fabio.lenherr@gmail.com>
2024-09-08 21:34:28 +0200

    Use nh

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -45,8 +45,9 @@
 
               set EDITOR "neovide --no-fork"
 
-              alias rebuild='sudo nixos-rebuild switch --flake ${config.conf.nixos-config-path}'
-              alias update='sudo nixos-rebuild switch --flake ${config.conf.nixos-config-path}'
+              alias rebuild='nh os switch'
+              alias update='nix flake update $FLAKE'
+              alias updateLock='nix flake lock $FLAKE --update-input'
               abbr --add ls 'lsd'
               abbr --add :q 'exit'
               abbr --add gh 'git push origin'
