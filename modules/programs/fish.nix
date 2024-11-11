5ada5903ef57496d12bfe840dd454720486adee3
DashieTM <fabio.lenherr@gmail.com>
2024-11-11 15:51:39 +0100

    Yazi alias fix

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -65,7 +65,7 @@
               abbr --add rm 'rip'
               abbr --add cp 'fcp'
               abbr --add cd 'z'
-              abbr --add yazi 'y'
+              abbr --add y 'yazi'
 
               set fish_greeting
               # pwd based on the value of _ZO_RESOLVE_SYMLINKS.
