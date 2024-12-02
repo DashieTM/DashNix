639195dc432ee161b0523d5bd490bf63860919f8
DashieTM <fabio.lenherr@gmail.com>
2024-12-02 09:51:24 +0100

    Update font

--- modules/programs/stylix.nix
+++ modules/programs/stylix.nix
@@ -74,7 +74,7 @@
         };
 
         monospace = {
-          package = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; });
+          package = pkgs.nerd-fonts.jetbrains-mono;
           name = "JetBrainsMono Nerd Font Mono";
         };
 
