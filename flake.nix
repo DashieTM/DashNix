5556e410aef954f503381d53a8744e86a1186c8d
DashieTM <fabio.lenherr@gmail.com>
2024-12-03 23:55:39 +0100

    Add caches

--- flake.nix
+++ flake.nix
@@ -106,7 +106,13 @@
       "https://hyprland.cachix.org"
       "https://anyrun.cachix.org"
       "https://cache.garnix.io"
-      "https://oxipaste.cachix.io"
+      "https://oxipaste.cachix.org"
+      "https://oxinoti.cachix.org"
+      "https://oxishut.cachix.org"
+      "https://oxidash.cachix.org"
+      "https://oxicalc.cachix.org"
+      "https://hyprdock.cachix.org"
+      "https://reset.cachix.org"
     ];
 
     extra-trusted-public-keys = [
@@ -114,6 +120,12 @@
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "oxipaste.cachix.org-1:n/oA3N3Z+LJP7eIWOwuoLd9QnPyZXqFjLgkahjsdDGc="
+      "oxinoti.cachix.org-1:dvSoJl2Pjo5HMaNngdBbSaixK9BSf2N8gzjP2MdGvfc="
+      "oxishut.cachix.org-1:axyAGF3XMh1IyMAW4UMbQCdMNovDH0KH6hqLLRJH8jU="
+      "oxidash.cachix.org-1:5K2FNHp7AS8VF7LmQkJAUG/dm6UHCz4ngshBVbjFX30="
+      "oxicalc.cachix.org-1:qF3krFc20tgSmtR/kt6Ku/T5QiG824z79qU5eRCSBTQ="
+      "hyprdock.cachix.org-1:HaROK3fBvFWIMHZau3Vq1TLwUoJE8yRbGLk0lEGzv3Y="
+      "reset.cachix.org-1:LfpnUUdG7QM/eOkN7NtA+3+4Ar/UBeYB+3WH+GjP9Xo="
     ];
   };
 }
