32e945b5f66849b1cd22c94d15f005f4054d28df
DashieTM <fabio.lenherr@gmail.com>
2026-05-17 12:03:59 +0200

    wip

--- modules/programs/greetd.nix
+++ modules/programs/greetd.nix
@@ -157,7 +157,7 @@
           hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
 
           hl.on("hyprland.start", function()
-            hl.dsp.exec_cmd("${pkgs.regreet}/bin/regreet --style /home/${username}/.config/gtk-3.0/gtk.css --config /home/${username}/.config/regreet/regreet.toml; hyprctl dispatch hl.dsp.exit()")
+            hl.dsp.exec_cmd("sh -c ${pkgs.regreet}/bin/regreet --style /home/${username}/.config/gtk-3.0/gtk.css --config /home/${username}/.config/regreet/regreet.toml; hyprctl dispatch hl.dsp.exi()")
           end)
         '';
 
